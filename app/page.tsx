6eab7c9f39c8358daf1a2afac51f457d148a3e5f
naif2500 <94239881+naif2500@users.noreply.github.com>
2024-08-20 13:39:41 +0200

    deleted more files

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from 'react';
 import CardSpread from '@/components/animata/card/card-spread';
 
@@ -7,16 +7,14 @@ const HeroSection = () => {
   return (
     <div className="hero bg-white min-h-[80vh]">
       <div className="hero-content flex-col lg:flex-row-reverse ">
-        {/* Replace Image with CardSpread Component */}
         <div className='max-w-sm'>
           <CardSpread />
         </div>
         
-        {/* Text and Call to Action */}
         <div className='lg:mr-20'>
           <h1 className=" text-5xl font-bold">Master Anything with <br/>Flashcards</h1>
           <p className="max-w-3xl py-6">
-          Unlock your learning potential with customizable flashcards. Whether you're studying for exams, learning a new language, or enhancing your skills, our tool helps you retain knowledge more effectively and efficiently.
+          Unlock your learning potential with customizable flashcards. Whether you&apos;re studying for exams, learning a new language, or enhancing your skills, our tool helps you retain knowledge more effectively and efficiently.
            
           </p>
           <button className="btn btn-primary">Get Started</button>
@@ -30,7 +28,6 @@ const Page = () => {
   return (
     <div>
       <HeroSection />
-      {/* Additional content can be added here */}
     </div>
   );
 };
